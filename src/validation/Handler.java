3a885ee1d389afc8dff7d7e3f2c98a69997eb118
Sean <seannevintsy@gmail.com>
2017-05-06 01:08:23 +0800

    DragonSMSFramework v2 initial commit

--- src/validation/Handler.java
+++
@@ -1,9 +0,0 @@
-package validation;
-import java.lang.reflect.Field;
-import java.lang.reflect.Method;
-
-
-public interface Handler 
-{
-	public void process(Object o, Method m, Object[] args) throws Exception;
-}
