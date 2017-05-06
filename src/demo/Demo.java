16111832b2685edabb5369cc523aadb0877b4d5d
Sean <seannevintsy@gmail.com>
2017-05-07 03:41:51 +0800

    Add JPA stuff

--- src/demo/Demo.java
+++ src/demo/Demo.java
@@ -1,10 +1,18 @@
 package demo;
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 import solution.Context;
 
 public class Demo {
 	public static void main(String[] args){
-		//Context c = new Context();
-		Context c = new Context("./src/demo/Input.txt");
+		AbstractApplicationContext ctx;
+    	
+        // load application context files
+        ctx = new ClassPathXmlApplicationContext(new String []{"applicationContext.xml", "applicationContext-jpa.xml"});
+		
+		Context c = new Context();
+		//Context c = new Context("./src/demo/Input.txt");
 		while(c.inputReader.readerNotFinished()){
 			System.out.println(c.inputReader.readLine());
 		}
--- src/solution/Context.java
+++ src/solution/Context.java
@@ -1,12 +1,26 @@
 package solution;
 
+import java.util.HashMap;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
 import smsframework.inputreader.InputReader;
+import solution.db.entity.User;
+import solution.db.repositories.UserRepository;
 import solution.states.NotRegisteredState;
 import solution.states.State;
 
+@Component
 public class Context {
+	HashMap<String,Object> args;
 	private State state;
 	public InputReader inputReader;
+	
+	@Autowired
+	UserRepository ur;
 	
 	public Context(){
 		state = new NotRegisteredState();
@@ -16,5 +30,10 @@ public class Context {
 	public Context(String path){
 		state = new NotRegisteredState();
 		inputReader = new InputReader(path);
+	}
+	
+	@PostConstruct
+	public void runDB(){
+		
 	}
 }
