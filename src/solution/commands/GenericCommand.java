0f892c939430aac8376aa7787ff03559d3550016
Sean <seannevintsy@gmail.com>
2017-05-08 00:06:16 +0800

    Minor change to GenericCommand's regex
    Regex now includes commands with no params (e.g. look)

--- src/solution/commands/GenericCommand.java
+++ src/solution/commands/GenericCommand.java
@@ -5,7 +5,7 @@ import java.util.regex.Matcher;
 import smsframework.annotations.Regex;
 import smsframework.annotations.RegexHandler;
 
-@Regex(regex="(?i)\\s*(?!\\b(go|register)\\b)(\\w+)\\s+(\\w+)\\s*")
+@Regex(regex="(?i)\\s*(?!\\b(go|register)\\b)(\\w+)(\\s+\\w+)?\\s*")
 public class GenericCommand implements RegexHandler{
 	@Override
 	public void process(Matcher m) throws Exception {
