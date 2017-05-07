08a708277ad2eb8e49cbdb3509ab05982063fc7c
Sean <seannevintsy@gmail.com>
2017-05-08 01:49:47 +0800

    Another minor GenericCommand regex fix

--- src/solution/commands/GenericCommand.java
+++ src/solution/commands/GenericCommand.java
@@ -5,10 +5,12 @@ import java.util.regex.Matcher;
 import smsframework.annotations.Regex;
 import smsframework.annotations.RegexHandler;
 
-@Regex(regex="(?i)\\s*(?!\\b(go|register)\\b)(\\w+)(\\s+\\w+)?\\s*")
+@Regex(regex="(?i)\\s*(?!\\b(go|register)\\b)(\\w+)(\\s+(\\w+))?\\s*")
 public class GenericCommand implements RegexHandler{
 	@Override
 	public void process(Matcher m) throws Exception {
 		System.out.println("generic");
+		System.out.println(m.group(2));
+		System.out.println(m.group(4));
 	}
 }
